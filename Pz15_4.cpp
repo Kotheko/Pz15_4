77ef81e93bcd63c39e0b28228512f7aa08fe2173
Ruslan Denisov <necronlive@mail.ru>
2024-01-21 13:21:36 +0900

    Initial

---
+++ Pz15_4.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+void FindOddNumbers(int n, bool IsOdd)
+{
+    if (IsOdd)
+    {
+        std::cout << n << std::endl;
+    }
+}
+
+int main()
+{
+    for (int i = 0; i <= 10; i++)
+    {
+        FindOddNumbers(i, i % 2 != 0);
+    }
+}
